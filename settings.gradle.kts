56a2e59b79f55ecc476b62923ca6b08b0bdbd3f7
Darinka2022 <darinka2020lino@gmail.com>
2023-11-01 05:42:29 -0500

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "AppMovil_fast_food"
+include(":app")
