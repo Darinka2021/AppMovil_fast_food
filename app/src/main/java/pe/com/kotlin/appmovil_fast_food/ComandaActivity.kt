a7f42e6a7ba7e559ee0f5c4729605715050b649c
Darinka2022 <darinka2020lino@gmail.com>
2023-11-23 01:05:33 -0500

    correcion de btn_totalizar en comanda

--- app/src/main/java/pe/com/kotlin/appmovil_fast_food/ComandaActivity.kt
+++ app/src/main/java/pe/com/kotlin/appmovil_fast_food/ComandaActivity.kt
@@ -1,9 +1,12 @@
 package pe.com.kotlin.appmovil_fast_food
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.Spinner
+import android.widget.Toast
 import pe.com.kotlin.appmovil_fast_food.implementacion.ImpComanda
 import pe.com.kotlin.appmovil_fast_food.interfaz.IComanda
 
@@ -14,17 +17,26 @@ class ComandaActivity : AppCompatActivity() {
     //declaramos adaptador
     private var adaptadorCategorias:ArrayAdapter<String>? = null
     private lateinit var comboCateg:Spinner
+    private lateinit var btnTotalizar:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_comanda)
 
         comboCateg=findViewById(R.id.sp_comanda_categoria)
+        btnTotalizar=findViewById(R.id.btn_totalizar_pedido)
 
         //modelamos la data en adaptador
         adaptadorCategorias = ArrayAdapter(this,android.R.layout.simple_spinner_item,comandas.CargarCategorias())
         //cargamos el spinner
         comboCateg.adapter = adaptadorCategorias
 
+
+        btnTotalizar.setOnClickListener {
+            Toast.makeText(this,"se calculará la compra ...",Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, DetalleActivity::class.java)
+            startActivity(intent)
+
+        }
 
     }
 }
