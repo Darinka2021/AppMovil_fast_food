1b85a96ef5f7ee840c1501601ff0a71403395e12
Darinka2022 <darinka2020lino@gmail.com>
2023-12-04 12:14:17 -0500

    actualizando ruta de api

--- app/src/main/java/pe/com/kotlin/appmovil_fast_food/remote/Api.kt
+++ app/src/main/java/pe/com/kotlin/appmovil_fast_food/remote/Api.kt
@@ -4,7 +4,7 @@ import pe.com.kotlin.appmovil_fast_food.service.CategoriaService
 
 object Api {
 
-    val API_URL="http://192.168.100.7:8097/categoria/"
+    val API_URL="http://192.168.100.7:8099/supertasty/categoria"
 
     val categoriaservice:CategoriaService? get()
     = RetrofitClient.getConnection(API_URL)?.create(CategoriaService::class.java)
